9652ccb84c662ddb00b7c9a73135e4c3988d759c
Chloé VIEILLE <37896835+ChloeViei@users.noreply.github.com>
2018-09-06 10:12:47 -0400

    Commencement de la gestion de la BDD postgre

--- src/accesseur/PaysDAO.java
+++ src/accesseur/PaysDAO.java
@@ -21,6 +21,41 @@ public class PaysDAO {
 
     
 	public List<Pays> listerPays(){
+		
+		String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+		String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/lieuDecouvrir";
+		String BASEDEDONNEES_USAGER = "postgres";
+		String BASEDEDONNEES_MOTDEPASSE = "root";
+
+
+        try {
+            Class.forName(BASEDEDONNEES_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+			Connection connection = DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
+
+            Statement requeteListePays = connection.createStatement();
+            ResultSet curseurListePays = requeteListePays.executeQuery("SELECT * FROM pays");
+
+            while (curseurListePays.next()){
+
+                String nom = curseurListePays.getString("nom");
+                String continent = curseurListePays.getString("continent");
+                String population = curseurListePays.getString("population");
+                String langue = curseurListePays.getString("langue");
+                String capitale = curseurListePays.getString("capital");
+
+                System.out.println("Pays : " + nom );
+
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return this.simulerListerPays();
     }
